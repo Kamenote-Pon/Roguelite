f3e4d4e71828fed4e71e7948aa754bef6a810197
Kamenote-Pon <ponmimi0204@gmail.com>
2026-07-18 18:23:14 +0900

    2026/07/18 授業終わり

--- Assets/Sclipt/InGame/Enemy/EnemyController.cs
+++ Assets/Sclipt/InGame/Enemy/EnemyController.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SocialPlatforms;
+using System;
 
 namespace TPSRoguelite.InGame.Enemy
 {
@@ -7,6 +11,9 @@ namespace TPSRoguelite.InGame.Enemy
     {
         //
         private const string PLAYER_TAG_NAME ="Player";
+
+        private const float KNOCKBACK_FOECE = 2.0f;
+        private const float KNOCKBACK_DURARION = 0.15f;
 
         //敵の本体
         [SerializeField] private EnemyState enemystate = null;
@@ -17,7 +24,8 @@ namespace TPSRoguelite.InGame.Enemy
         //目的地となるPlayerのTransform
         private Transform targetPlayer = null;
 
-        //
+        //ノックバック動作のキャンセルトークン
+        private CancellationTokenSource hitCts;
 
         private void Awake()
         {
@@ -49,6 +57,61 @@ namespace TPSRoguelite.InGame.Enemy
                 //プレイヤーの現在位置を毎フレーム目的地としてセット
                 navMeshAgent.SetDestination(targetPlayer.position);
             }
+        }
+
+        private void OnEnable()
+        {
+            if (enemystate != null)
+            {
+                enemystate.OnDamageAction -= HandleDamage;
+                enemystate.OnDamageAction += HandleDamage;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if(enemystate != null)
+            {
+                enemystate.OnDamageAction -= HandleDamage;
+            }
+            if (navMeshAgent != null && navMeshAgent.isActiveAndEnabled)
+            {
+                navMeshAgent.isStopped = false;
+            }
+        }
+
+        private async UniTaskVoid KnockbackAsync(CancellationToken token)
+        {
+            if(navMeshAgent == null)
+            {
+                return;
+            }
+            bool wasStopped = navMeshAgent.isStopped;
+            navMeshAgent.isStopped = true;
+
+            if(targetPlayer != null)
+            {
+                Vector3 dir = (transform.position - targetPlayer.position).normalized;
+                dir.y = 0;
+                transform.position += dir * KNOCKBACK_FOECE;
+            }
+            bool isCanceled = await UniTask.Delay(TimeSpan.FromSeconds(KNOCKBACK_DURARION), cancellationToken: token).SuppressCancellationThrow();
+
+            if (!isCanceled && navMeshAgent.isActiveAndEnabled)
+            {
+                navMeshAgent.isStopped = wasStopped;
+            }
+        }
+        private void HandleDamage()
+        {
+            hitCts?.Cancel();
+            hitCts?.Dispose();
+            hitCts = null;
+
+            hitCts = new CancellationTokenSource();
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(hitCts.Token, this.GetCancellationTokenOnDestroy());
+
+            KnockbackAsync(linkedCts.Token).Forget();
         }
     }
 }
--- Assets/Sclipt/InGame/Enemy/EnemyState.cs
+++ Assets/Sclipt/InGame/Enemy/EnemyState.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 using Core.InterFace;
 using UnityEngine.Events;
 using Core.MasterData;
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Rendering.Universal;
 
 namespace TPSRoguelite.InGame.Enemy 
 {
     public class EnemyState : MonoBehaviour,IDamageable
     {
+        //点滅時間
+        private const float FLASH_DURATION = 0.1f;
+
+        //キャラクターのレンダラー
+        [SerializeField] private Renderer[] modeRenderers;
+
+        //キャラクターの元々の色
+        private Color[] defaultColors;
+
+        //点滅するアニメーションのキャンセルトークン
+        private CancellationTokenSource flashCts;
+
         //敵のデータ
          public EnemyDataRecord EnemyDataAsset { get;private set; }
 
@@ -15,10 +31,24 @@ namespace TPSRoguelite.InGame.Enemy
 
 
         public event UnityAction<EnemyState> OnRetuenToPoolAction;
+
+        public event UnityAction OnDamageAction;
         
         public void Initialize(ulong id)
         {
             EnemyDataAsset = MasterDataAccessor.Instance.GetById<EnemyDataRecord>(id);
+
+            if(modeRenderers != null)
+            {
+                defaultColors = new Color[modeRenderers.Length];
+                for(int i = 0; i < modeRenderers.Length; i++)
+                {
+                    if(modeRenderers[i] != null)
+                    {
+                        defaultColors[i] = modeRenderers[i].material.color;
+                    }
+                }
+            }
         }
 
         public void SetUp()
@@ -30,6 +60,7 @@ namespace TPSRoguelite.InGame.Enemy
             }
             CurrentHP = EnemyDataAsset.MaxHP;
             gameObject.SetActive(true);
+            ResetColor();
         }
 
         public void TakeDamage(int damageAmount)
@@ -43,7 +74,20 @@ namespace TPSRoguelite.InGame.Enemy
             CurrentHP -= damageAmount;
             Debug.Log($"{EnemyDataAsset.EnemyName}に{damageAmount}のダメージ!残りHP:{CurrentHP}");
 
-            if (CurrentHP <= 0)
+            if(CurrentHP > 0)
+            {
+                OnDamageAction?.Invoke();
+
+                flashCts?.Cancel();
+                flashCts?.Dispose();
+                flashCts = null;
+
+                flashCts = new CancellationTokenSource();
+                var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(flashCts.Token, this.GetCancellationTokenOnDestroy());
+
+                DamageFlashAsync(linkedCts.Token).Forget();
+            }
+            else
             {
                 Die();
             }
@@ -54,6 +98,43 @@ namespace TPSRoguelite.InGame.Enemy
             Debug.Log($"{EnemyDataAsset.EnemyName}を倒しました");
             gameObject.SetActive(false);
             OnRetuenToPoolAction?.Invoke(this);
+        }
+
+        //色をリセット
+        private void ResetColor()
+        {
+            if(modeRenderers == null || defaultColors == null)
+            {
+                return;
+            }
+            for(int i = 0;i < modeRenderers.Length; i++)
+            {
+                if(modeRenderers[i] != null)
+                {
+                    modeRenderers[i].material.color = defaultColors[i];
+                }
+            }
+        }
+
+        private async UniTaskVoid DamageFlashAsync(CancellationToken token)
+        {
+            if (modeRenderers == null)
+            {
+                return;
+            }
+            foreach(var renderer in modeRenderers)
+            {
+                if(renderer != null)
+                {
+                    renderer.material.color = Color.red;
+                }
+            }
+
+            bool isCanceled = await UniTask.Delay(TimeSpan.FromSeconds(FLASH_DURATION), cancellationToken : token).SuppressCancellationThrow();
+            if (!isCanceled)
+            {
+                ResetColor();
+            }
         }
     }
 }
